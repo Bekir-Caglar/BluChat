9a818edecb5f3fe4e9b278a84e5253bc28e26b86
Bekir Çağlar <uyibey@gmail.com>
2024-10-17 00:05:00 +0300

    feat: added play video when user send video

--- app/src/main/java/com/bekircaglar/bluchat/presentation/message/camera/SendTakenPhotoScreen.kt
+++ app/src/main/java/com/bekircaglar/bluchat/presentation/message/camera/SendTakenPhotoScreen.kt
@@ -1,15 +1,15 @@
 package com.bekircaglar.bluchat.presentation.message.camera
 
-import VideoThumbnailComposable
+import androidx.annotation.OptIn
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Icon
@@ -27,58 +27,98 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.media3.common.MediaItem
+import androidx.media3.common.util.UnstableApi
+import androidx.media3.datasource.DefaultHttpDataSource
+import androidx.media3.exoplayer.ExoPlayer
+import androidx.media3.exoplayer.source.MediaSource
+import androidx.media3.exoplayer.source.ProgressiveMediaSource
 import androidx.navigation.NavController
 import coil.compose.rememberImagePainter
-import com.bekircaglar.bluchat.utils.IMAGE
+import com.bekircaglar.bluchat.Player
 import com.bekircaglar.bluchat.R
 import com.bekircaglar.bluchat.navigation.Screens
 import com.bekircaglar.bluchat.presentation.message.component.MessageTextField
+import com.bekircaglar.bluchat.utils.IMAGE
 
+@OptIn(UnstableApi::class)
 @Composable
-fun SendTakenPhotoScreen(imageUrl: String, chatId: String,navController: NavController) {
+fun SendTakenPhotoScreen(imageUrl: String, chatId: String, navController: NavController) {
     val viewModel: SendTakenPhotoViewModel = hiltViewModel()
     var message by remember { mutableStateOf("") }
     val context = LocalContext.current
+    val mediaItem: MediaItem
+    if (imageUrl.contains(".mp4")) {
+        mediaItem = imageUrl.let { MediaItem.fromUri(it) }
+    } else {
+        mediaItem = MediaItem.EMPTY
+    }
+
+    val mediaSource: MediaSource = remember {
+        ProgressiveMediaSource.Factory(DefaultHttpDataSource.Factory())
+            .createMediaSource(mediaItem ?: MediaItem.EMPTY)
+    }
+    val exoPlayer = remember {
+        ExoPlayer.Builder(context).build().apply {
+            setMediaSource(mediaSource)
+            playWhenReady = true
+            prepare()
+        }
+    }
 
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .background(Color.Black),
+            .background(Color.Black)
+            .padding(16.dp),
+        verticalArrangement = Arrangement.SpaceBetween,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        if (imageUrl.contains(".mp4")){
-            VideoThumbnailComposable(context = context, videoUrl =  imageUrl, isShapeShouldSquare = false, onVideoClick = {})
-        }
-        Image(
-            painter = rememberImagePainter(imageUrl),
-            contentDescription = null,
+        Box(
             modifier = Modifier
                 .weight(1f)
                 .fillMaxSize(),
-            contentScale = ContentScale.Fit
-        )
-        Spacer(modifier = Modifier.height(16.dp))
-        Row(verticalAlignment = Alignment.CenterVertically) {
+            contentAlignment = Alignment.Center
+        ) {
+            if (imageUrl.contains(".mp4")) {
+                Player(exoPlayer = exoPlayer)
+            } else {
+                Image(
+                    painter = rememberImagePainter(imageUrl),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .fillMaxSize(),
+                    contentScale = ContentScale.Fit
+                )
+            }
+        }
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = 16.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center
+        ) {
             MessageTextField(
                 searchText = message,
                 onSearchTextChange = { newText ->
-                    message =
-                        newText.replaceFirstChar { if (it.isLowerCase()) it.titlecase() else it.toString() }
+                    message = newText.replaceFirstChar { if (it.isLowerCase()) it.titlecase() else it.toString() }
                 },
                 onSend = {
                     viewModel.sendMessage(
-                        imageUrl = imageUrl.toString(),
+                        imageUrl = imageUrl,
                         message = message,
                         messageType = IMAGE,
                         chatId = chatId,
                     )
                     navController.navigate(Screens.MessageScreen.createRoute(chatId))
-
                 },
                 placeholderText = "Add a caption...",
-                modifier = Modifier.fillMaxWidth(0.7f)
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(end = 8.dp)
             )
-            Spacer(modifier = Modifier.padding(16.dp))
             Icon(
                 painter = painterResource(id = R.drawable.ic_send),
                 contentDescription = null,
@@ -87,17 +127,14 @@ fun SendTakenPhotoScreen(imageUrl: String, chatId: String,navController: NavCont
                     .size(30.dp)
                     .clickable {
                         viewModel.sendMessage(
-                            imageUrl = imageUrl.toString(),
+                            imageUrl = imageUrl,
                             message = message,
                             messageType = IMAGE,
                             chatId = chatId,
                         )
                         navController.navigate(Screens.MessageScreen.createRoute(chatId))
-
                     }
             )
         }
-        Spacer(modifier = Modifier.height(32.dp))
-
     }
 }
